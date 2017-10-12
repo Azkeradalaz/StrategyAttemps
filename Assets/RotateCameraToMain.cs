8affe174aae1443cc70b656e8d067f836e2482fc
Azkeradalaz <daazwaa@gmail.com>
2017-10-12 14:50:57 +0300

    Added projetct.
    0 project. Has a cool star tho.

---
+++ Assets/RotateCameraToMain.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class RotateCameraToMain : MonoBehaviour {
+
+	public Camera mainCamera;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		gameObject.transform.rotation = mainCamera.transform.rotation;
+	}
+}
